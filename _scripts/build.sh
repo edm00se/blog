9934a7f948a9f4c5e5e5764e711a03023454a595
Eric McCormick <edm00se@gmail.com>
2019-12-22 23:49:50 -0600

    rebuilt Gemfile.lock

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gem install bundler
+gem install bundler:2.1.2
 bundle exec jekyll build --config _config.yml
 npm init -y && npm install --no-save workbox-build && node _config/workbox-build.js
