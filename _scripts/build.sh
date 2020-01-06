81ec2d06d6afe614af613d4ba44ac96ded07a528
Eric McCormick <edm00se@gmail.com>
2020-01-05 22:53:13 -0600

    use latest bundler
    2.1.4 as of today

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gem install bundler:2.1.2
+gem install bundler
 bundle exec jekyll build --config _config.yml
 npm init -y && npm install --no-save workbox-build && node _config/workbox-build.js
