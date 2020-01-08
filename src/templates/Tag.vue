64d695f09ff9e40be0648e3002863def6c8bea8b
Eric McCormick <edm00se@gmail.com>
2020-01-08 15:38:49 -0600

    wires tag name into page title

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -41,8 +41,10 @@ export default {
     Author,
     PostCard
   },
-  metaInfo: {
-    title: 'Hello, world!'
+  metaInfo() {
+    return {
+      title: `'${this.$page.tag.title}' posts`
+    };
   }
 }
 </script>
