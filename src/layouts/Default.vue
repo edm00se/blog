d8a43215d099161ec7ecc64784655b910311d309
Eric McCormick <edm00se@gmail.com>
2020-01-08 21:37:38 -0600

    enforces https in url

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -17,7 +17,7 @@
 
     <footer class="footer">
       <span class="footer__copyright">Copyright © {{ new Date().getFullYear() }}. </span>
-      <span class="footer__links">Powered by <a href="//gridsome.org"> Gridsome </a></span>
+      <span class="footer__links">Powered by <a href="https://gridsome.org"> Gridsome </a></span>
     </footer>
 
   </div>
