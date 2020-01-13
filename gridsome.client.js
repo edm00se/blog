2790cafa96a09ffeee1b33311d1a648704328e94
Eric McCormick <edm00se@gmail.com>
2020-01-13 15:19:30 -0600

    adds custom analytics package

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,6 +1,6 @@
+import VueAnalytics from 'vue-analytics';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { config, library } from '@fortawesome/fontawesome-svg-core';
-// import { faTag, faTags, faRss, faHome, faAddressCard } from '@fortawesome/free-solid-svg-icons';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 
@@ -9,4 +9,12 @@ library.add(faLink, faTwitter, faGithub);
 
 export default function (Vue) {
   Vue.component('font-awesome', FontAwesomeIcon);
+  Vue.use(VueAnalytics, {
+    id: process.env.GRIDSOME_ANALYTICS_ID,
+    customResourceURL: '/analyzer.js',
+    debug: {
+      sendHitTask: process.env.NODE_ENV === 'production'
+    },
+    disabled: process.isServer
+  })
 }
