56bbb512101f3c0830c89d5af26bcf910768c14f
Eric McCormick <edm00se@gmail.com>
2021-02-01 13:25:27 -0600

    moar logs!

--- src/main.js
+++ src/main.js
@@ -15,17 +15,31 @@ if (process.isClient && process.env.NODE_ENV === 'production' && 'serviceWorker'
   console.log('service worker time baby!');
   const wb = new Workbox('/sw.js');
   wb.addEventListener('installed', event => {
-    console.log('sw installged, event.isUpdate? ', event.isUpdate);
+    console.log('sw installed, event.isUpdate? ', event.isUpdate);
   });
-  wb.addEventListener('message', (event) => {
-    console.log('message: ', event);
-    if (event.data.type === 'CACHE_UPDATED') {
-      const {updatedURL} = event.data.payload;
-  
-      if (window.confirm(`New content for ${updatedURL} is available. Reload to view the latest?`)) {
-        window.location.reload();
-      }
+  wb.addEventListener('activated', (event) => {
+    console.log('sw activated, event.isUpdate? ', event.isUpdate);
+    if (!event.isUpdate) {
+      console.log('Service worker activated for the first time!');
     }
   });
+  wb.addEventListener('waiting', (event) => {
+    console.log(`A new service worker has installed, but it can't activate` +
+        `until all tabs running the current version have fully unloaded.`);
+  });
+  wb.addEventListener('installing', (event) => {
+    console.log('new content on its way');
+  });
+    
+  // wb.addEventListener('message', (event) => {
+  //   console.log('message: ', event);
+  //   if (event.data.type === 'CACHE_UPDATED') {
+  //     const {updatedURL} = event.data.payload;
+  
+  //     if (window.confirm(`New content for ${updatedURL} is available. Reload to view the latest?`)) {
+  //       window.location.reload();
+  //     }
+  //   }
+  // });
   wb.register();
 }
