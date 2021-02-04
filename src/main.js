553a06f682d52b1dec41089f07e334d5cc60344d
Eric McCormick <edm00se@gmail.com>
2021-02-04 11:58:29 -0600

    Revert "move sw registration"
    This reverts commit b4fc85aebb643f182db2d3f753489188b5fb24a5.

--- src/main.js
+++ src/main.js
@@ -10,36 +10,36 @@ export default function(Vue, { router, head, isClient }) {
   Vue.component('Layout', DefaultLayout);
 }
 
-// if (process.isClient && process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-//   const {Workbox} = require('workbox-window');
-//   console.log('service worker time baby!');
-//   const wb = new Workbox('/sw.js');
-//   wb.addEventListener('installed', event => {
-//     console.log('sw installed, event.isUpdate? ', event.isUpdate);
-//   });
-//   wb.addEventListener('activated', (event) => {
-//     console.log('sw activated, event.isUpdate? ', event.isUpdate);
-//     if (!event.isUpdate) {
-//       console.log('Service worker activated for the first time!');
-//     }
-//   });
-//   wb.addEventListener('waiting', (event) => {
-//     console.log(`A new service worker has installed, but it can't activate` +
-//         `until all tabs running the current version have fully unloaded.`);
-//   });
-//   wb.addEventListener('installing', (event) => {
-//     console.log('new content on its way');
-//   });
-
-// wb.addEventListener('message', (event) => {
-//   console.log('message: ', event);
-//   if (event.data.type === 'CACHE_UPDATED') {
-//     const {updatedURL} = event.data.payload;
-
-//     if (window.confirm(`New content for ${updatedURL} is available. Reload to view the latest?`)) {
-//       window.location.reload();
-//     }
-//   }
-// });
-//   wb.register();
-// }
+if (process.isClient && process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  const {Workbox} = require('workbox-window');
+  console.log('service worker time baby!');
+  const wb = new Workbox('/sw.js');
+  wb.addEventListener('installed', event => {
+    console.log('sw installed, event.isUpdate? ', event.isUpdate);
+  });
+  wb.addEventListener('activated', (event) => {
+    console.log('sw activated, event.isUpdate? ', event.isUpdate);
+    if (!event.isUpdate) {
+      console.log('Service worker activated for the first time!');
+    }
+  });
+  wb.addEventListener('waiting', (event) => {
+    console.log(`A new service worker has installed, but it can't activate` +
+        `until all tabs running the current version have fully unloaded.`);
+  });
+  wb.addEventListener('installing', (event) => {
+    console.log('new content on its way');
+  });
+    
+  // wb.addEventListener('message', (event) => {
+  //   console.log('message: ', event);
+  //   if (event.data.type === 'CACHE_UPDATED') {
+  //     const {updatedURL} = event.data.payload;
+  
+  //     if (window.confirm(`New content for ${updatedURL} is available. Reload to view the latest?`)) {
+  //       window.location.reload();
+  //     }
+  //   }
+  // });
+  wb.register();
+}
