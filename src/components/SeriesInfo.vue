9c8079b46cc016e823e2b85efae8eb8d2b2a9ba9
Eric McCormick <edm00se@users.noreply.github.com>
2023-06-09 18:50:50 +0000

    adds hover text on you are here icon

--- src/components/SeriesInfo.vue
+++ src/components/SeriesInfo.vue
@@ -7,7 +7,7 @@
       <ul class="series-list">
         <li v-for="post in orderedSeries" :key="post.node.id">
           <span v-if="post.node.id == current">
-            <font-awesome :icon="['fas', 'chevron-right']" :class="'w-5 sm:w-6 fill-current mx-auto mb-1'" size="3x" style="vertical-align: middle;color: var(--link-color);" />
+            <font-awesome title="you are here" :icon="['fas', 'chevron-right']" :class="'w-5 sm:w-6 fill-current mx-auto mb-1'" size="3x" style="vertical-align: middle;color: var(--link-color);" />
             &nbsp;
           </span>
           <g-link class="post-tags__link" :to="post.node.path" :disabled="current === post.node.id">
